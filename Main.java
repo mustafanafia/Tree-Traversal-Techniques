c5104e8d985e61138bdbffc3f6fc80274fd1ca74
mustafanafia <55895079+mustafanafia@users.noreply.github.com>
2024-04-04 16:41:52 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,16 @@
+/*
+A Tree Data Structure can be traversed in following ways:
+
+    ~ Depth First Search or DFS
+    Inorder Traversal
+    Preorder Traversal
+    Postorder Traversal
+
+    ~ Level Order Traversal or Breadth First Search or BFS
+    Boundary Traversal
+    Diagonal Traversal
+*/
+
 public class TreeNode {
 
     public int val;
